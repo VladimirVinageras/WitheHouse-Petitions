d6af193facd5e11c8289f8eba5fc994e4ffaa299
VladimirVinageras <vvinageras@gmail.com>
2023-03-02 11:16:03 +0200

    Add files via upload

---
+++ project7/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  project7
+//
+//  Created by Vladimir Vinageras on 11.02.2023.
+//
+
+import Foundation
+
+
+struct Petition: Codable{
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ project7/Petitions.swift
@@ -0,0 +1,14 @@
+//
+//  Petitions.swift
+//  project7
+//
+//  Created by Vladimir Vinageras on 11.02.2023.
+//
+
+import Foundation
+
+struct Petitions: Codable{
+    var results: [Petition]
+    
+    
+}
